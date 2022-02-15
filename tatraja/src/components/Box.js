9ad4b768dd6743dd0f34a971df6e345d1690cd06
JRoshthen1 <jroshthen1@gmail.com>
2022-02-15 22:07:15 +0100

    Navbar creation, not finished toggle

--- tatraja/src/components/Box.js
+++ tatraja/src/components/Box.js
@@ -3,8 +3,8 @@ import React from 'react'
 function Box() {
   return (
     <mesh rotation={[90 , 0 , 20 ]}>
-        <boxBufferGeometry attach='geometry' args={[3 , 3 , 3]}/>
-        <meshLambertMaterial attach='material' color="gray"/>  
+        <boxBufferGeometry attach='geometry' args={[2 , 2 , 2 ]}/>
+        <meshLambertMaterial attach='material' color='#da759b'/>  
     </mesh>
   )
 }
