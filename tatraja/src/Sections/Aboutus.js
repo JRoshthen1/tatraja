76bc96690855388f0d6aa7a1f27ddb0eff5527d0
Martin Jaros <37882816+JRoshthen1@users.noreply.github.com>
2022-02-22 10:45:34 +0100

    Revert "some carousel"

--- tatraja/src/Sections/Aboutus.js
+++ tatraja/src/Sections/Aboutus.js
@@ -1,12 +1,7 @@
 import ABOUT from '../images/ABOUT.png'
 import Camera from '../images/Camera.png'
-import Flickity from 'react-flickity-component'
 
 const Aboutus = () => {
-  
-  const flickityOptions = {
-    initialIndex: 2
-}
 
   return (
     <div className="bg-no-repeat bg-cover statik h-auto" style={{
@@ -14,18 +9,7 @@ const Aboutus = () => {
     }}>
         <div className='flex flex-col justify-between align-middle pt-44'>
         <h1 className="text-black text-6xl text-center place-content-center font-frederika">About us</h1>
-        <Flickity
-      className={'carousel'} // default ''
-      elementType={'div'} // default 'div'
-      options={flickityOptions} // takes flickity options {}
-      disableImagesLoaded={false} // default false
-      reloadOnUpdate // default false
-      static // default false
-    >
-      <img src={Camera}/>
-      <img src={Camera}/>
-      <img src={Camera}/>
-    </Flickity>
+        <img className='relative  place-self-center' src={Camera}></img>
         <p className='pt-5 pb-10 text-xl w-80 text-center place-self-center'>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before the final copy is available.</p>
         </div>
         </div>
