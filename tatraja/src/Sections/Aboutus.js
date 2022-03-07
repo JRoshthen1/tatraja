54fa65e3e37fd9781c7943204a663f60a548740c
Dppriest555 <ivan.velkov01@abv.bg>
2022-03-07 13:05:06 +0100

    Roadmap replaces Carousel

--- tatraja/src/Sections/Aboutus.js
+++ tatraja/src/Sections/Aboutus.js
@@ -3,12 +3,17 @@ import Carousel from '../components/Carousel';
 
 
 
+
 const Aboutus = () => {
 
+  
+
+
+  //learn how this was made at https://www.youtube.com/watch?v=ersN5fk8py0 
+
+
   return (
-    <div className="bg-no-repeat bg-cover statik h-auto" style={{
-        backgroundImage: `url(${ABOUT})`
-    }}>
+    <div className="bg-no-repeat bg-cover statik h-auto">
         <div className='flex flex-col justify-between align-middle pt-44'>
         <h1 className="text-black text-6xl text-center place-content-center font-frederika">About us</h1>
         <p className='text-darkBlue pt-5 pb-10 text-xl w-80 text-center place-self-center'>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before the final copy is available.</p>
--- tatraja/src/components/Carousel.js
+++ tatraja/src/components/Carousel.js
@@ -1,39 +1,80 @@
 import '../slideshow.css';
 import React from 'react'
 import imagetest from '../images/Camera.png'
-import { useEffect } from 'react'
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import {MotionPathPlugin } from "gsap/MotionPathPlugin";
+import { useRef, useEffect } from 'react'
+
 
 
 function Carousel() {
 
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = "http://127.0.0.1:5500/script.js";
-    script.async = true;
-    document.body.appendChild(script);
-    console.log(script.src)
-  return () => {
-      document.body.removeChild(script);
-  
+useEffect(() => {
+  gsap.registerPlugin(ScrollTrigger, MotionPathPlugin);
+
+  gsap.defaults({ease: "none"});
+
+  const pulses = gsap.timeline({
+    defaults: {
+      scale: 2,
+      autoAlpha:1,
+      transformOrigin: 'center', 
+      ease: "elastic(2.5, 1)"
+    }})
+  .to( ".ball02",  {}, 0.84) 
+  .to(".ball03, .text02", {}, 1.36)
+  .to(".ball04, .text03", {}, 1.92)
+
+  const main = gsap.timeline({
+    scrollTrigger: {
+      trigger: "#svg",
+      scrub: true,
+      start: "top center",
+      end: "bottom center"
     }
-  }, []);
+  })
+  .to(".ball01" , {autoAlpha:1, duration:0.05})
+  .from(".theLine", { duration:4}, 0)
+  .to(".ball01", {motionPath:{
+    path:".theLine",
+    align:".theLine",
+    alignOrigin:[0.5, 0.5], 
+  }, duration:4}, 0)
+  .add(pulses, 0)
+}, [])
+
+   
+
   
-
-
   return (
-    <div className='relative'>
-    <div className="mainbox flex flex-row">
-  <div id='carousel' className="carousel">
-    <div className="item a">A</div>
-    <div className="item b">B</div>
-    <div className="item c">C</div>
-    <div className="item d">D</div>
-    <div className="item e">E</div>
-    <div className="item f">F</div>
-  </div>
-</div>
-<div className="next">Next</div>
-<div className="prev">Prev</div>
+    <div className='relative w-screen'>
+    <svg className=' w-3/4' id="svg" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 600 1200">
+  <path className="line01 line" d="M 10 200  600 200" ></path>
+  <path className="line02 line" d="M 10 400  600 400" ></path>
+  <path className="line03 line" d="M 10 600  600 600" ></path>
+  <path className="line04 line" d="M 10 800  600 800" ></path>
+  <path className="line05 line" d="M 10 1000  600 1000" ></path>
+  <text className="text01" x="30" y="190">2018</text>
+  <text className="text02" x="30" y="390">2019</text>
+  <text className="text03" x="30" y="590">2020</text>
+
+  <path className="theLine" 
+        d="M -5,0
+           Q 450 230 300 450 
+           T 130 750
+           Q 100 850 300 1000
+           T 150 1200"
+        fill="none" stroke="white" stroke-width="10px" />
+  
+  
+  
+  <circle className="ball ball01" r="20" cx="50" cy="100"></circle>
+  <circle className="ball ball02" r="20" cx="278" cy="201"></circle>
+  <circle className="ball ball03" r="20" cx="327" cy="401"></circle>
+  <circle className="ball ball04" r="20" cx="203" cy="601"></circle>
+
+</svg>
     </div>
     
   )
