79e7d5417e05e467572faaf04fc0598b81eaaa87
Dppriest555 <ivan.velkov01@abv.bg>
2022-03-10 21:05:20 +0100

    responsive Parralax effect

--- tatraja/src/Sections/HeroSection.js
+++ tatraja/src/Sections/HeroSection.js
@@ -4,17 +4,22 @@ import '../Hero.css';
 import gsap from 'gsap'
 
 import Mountains from '../svg/hero/Mountains.svg'
-import Clouds from '../svg/hero/Clouds.svg'
+import Cloud from '../svg/hero/Clouds.svg'
 import Foreground from '../svg/hero/ForeGround.svg'
+import smfr from '../svg/hero/sm-fr.svg'
+import smmt from '../svg/hero/sm-mt.svg'
 
 function HeroSection() {
 
   return (
-  <div className='wrapper flex flex-col'>
-   <div className="parallax">
-    <div className='clouds'> <img src={Clouds}></img> </div>
-    <div className='midground '> <img src={Mountains}></img> </div>
-    <div className='foreground'> <img src={Foreground}></img> </div>
+  <div className='wrapper '>
+   <div className="parallax flex flex-col">
+     <h1 className='heading absolute text-8xl font-cinzel font-bold text-black'>TATRAJA ART</h1>
+    <div className='cloud'><img src={Cloud}></img> </div>
+    <div className='midground'><img src={Mountains}></img> </div>
+    <div className='foreground'><img src={Foreground}></img> </div>
+    <div className='midground-sm'><img src={smfr}></img> </div>
+    <div className='foreground-sm'><img src={smmt}></img> </div>
 </div>
 </div>
   )
